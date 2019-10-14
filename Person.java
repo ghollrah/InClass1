a7657c178a88ea570a364210c1ad6cf1c3a78696
ghollrah <gretchen.hollrah-1@ou.edu>
2019-10-14 13:44:28 -0500

    added person.java

---
+++ Person.java
@@ -0,0 +1,27 @@
+
+public class Person extends NameHolder {
+
+	private String lastName;
+	
+	public Person() throws NameException {
+		String temp1 = "Bo";
+		String temp2 = "Smith";
+		lastName = temp1;
+		super.setFirstName(temp2);
+	}
+	
+	public Person(String firstName, String lastName) throws NameException {
+		this.lastName = lastName;
+		super.setFirstName(firstName);
+		
+	}
+	
+	@Override
+	public String describeSelf() {
+		// TODO Auto-generated method stub
+		String description = "*** First Name: " + super.getFirstName()
+				+ "Last Name: " + lastName + "***";
+		return description;
+	}
+
+}
