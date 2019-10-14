9d37d97f389591199a1725a40339141cb8c1f254
ghollrah <gretchen.hollrah-1@ou.edu>
2019-10-14 14:22:53 -0500

    Implement one test for Student

--- Student.java
+++ Student.java
@@ -1,4 +1,27 @@
 
-public class Student {
+public class Student extends Person implements Comparable<Student> {
+	
+	private String className;
+	private Integer rating;
+	private static final int INITIAL_RATING = 5;
 
+	
+	public Student() throws NameException {
+		super();
+		rating = INITIAL_RATING;
+		
+	}
+
+	
+	@Override
+	public int compareTo(Student o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public Integer getRating() {
+		return new Integer(rating);
+	}
+	
+	
 }
